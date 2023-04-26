27c49b43fb2aa40fbb41162b91f4c17748c62c95
Carlos Febres <carlosfebres@dorg.tech>
2023-04-26 11:53:46 -0400

    sponsor implementation

--- src/hooks/useGasless.js
+++ src/hooks/useGasless.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { ExecutionLib } from "../helpers/executionLib";
 import { SignatureTransfer } from "@uniswap/permit2-sdk";
 
 const REACT_APP_RELAYER_URL = process.env.REACT_APP_RELAYER_URL;
@@ -31,8 +32,13 @@ const EIP2612_TYPES = {
 };
 
 const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+const SPONSOR_ADDRESS = process.env.REACT_APP_SPONSOR_ADDRESS;
+
+// 5 tokens
+const SPONSOR_FEE_AMOUNT = ethers.constants.WeiPerEther.mul(5);
 
 const ERC20_PERMIT_TOKEN_ABI = [
+  "function transfer(address _to, uint256 _value) public returns (bool success)",
   "function nonces(address owner) external view returns (uint256)",
   "function allowance(address owner, address spender) external view returns (uint256)",
 ];
@@ -47,8 +53,8 @@ function getTimestamp(add = 0) {
 }
 
 class Relayer {
-  static async initialize(_token, signer) {
-    const erc20 = Relayer.#getContract(ECO_TOKEN, signer);
+  static async initialize(signer) {
+    const erc20 = Relayer.#getContract(signer);
 
     const address = await signer.getAddress();
     const network = await signer.provider.getNetwork();
@@ -74,7 +80,7 @@ class Relayer {
 
     const payload = { ...message, ...signature };
 
-    const response = await Relayer.#request(`/relayer/permit2/${_token}/init`, payload);
+    const response = await Relayer.#request(`/relayer/permit2/eco/init`, payload);
 
     // Wait 3s while indexing transaction
     await new Promise(resolve => setTimeout(resolve, 3000));
@@ -86,15 +92,11 @@ class Relayer {
     return response;
   }
 
-  static async #getNonce() {
-    return ethers.BigNumber.from(ethers.utils.randomBytes(32)).toString();
-  }
-
-  static async transfer(_token, signer, to, amount) {
+  static async transfer(signer, to, amount) {
     const from = await signer.getAddress();
     const network = await signer.provider.getNetwork();
 
-    const nonce = await Relayer.#getNonce(ECO_TOKEN, signer);
+    const nonce = Relayer.#getNonce();
     const spender = await Relayer.#getRelayerAddress();
     const deadline = getTimestamp(1800).toString();
 
@@ -109,7 +111,7 @@ class Relayer {
     const signature = await signer._signTypedData(domain, types, values);
 
     const payload = { from, to, permit, signature };
-    const response = await Relayer.#request(`/relayer/permit2/${_token}/transfer`, payload);
+    const response = await Relayer.#request(`/relayer/permit2/eco/transfer`, payload);
 
     // Wait 3s while indexing transaction
     await new Promise(resolve => setTimeout(resolve, 3000));
@@ -121,15 +123,67 @@ class Relayer {
     return response;
   }
 
+  static async sponsorTransfer(signer, to, amount) {
+    const execution = await Relayer.#getExecutionForSingleTransfer(signer, to, amount);
+    const network = await signer.provider.getNetwork();
+
+    const permit = {
+      spender: SPONSOR_ADDRESS,
+      permitted: execution.tokens,
+      nonce: execution.nonce,
+      deadline: execution.deadline,
+    };
+
+    const { domain } = SignatureTransfer.getPermitData(permit, PERMIT2_ADDRESS, network.chainId);
+    const message = ExecutionLib.hashTypedData(
+      domain,
+      ExecutionLib.hashWithWitness(permit, ExecutionLib.hash(execution)),
+    );
+
+    // Avoid messageHash function
+    execution.signature = ethers.utils.joinSignature(signer._signingKey().signDigest(message));
+
+    const payload = { sponsor: SPONSOR_ADDRESS, execution };
+    const response = await Relayer.#request(`/relayer/sponsor/eco/transfer`, payload);
+
+    // Wait 3s while indexing transaction
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    const tx = await signer.provider.getTransaction(response.transaction.hash);
+    if (tx) await tx.wait();
+    return response;
+  }
+
   static async isEnabled(_token, signer) {
-    const erc20 = Relayer.#getContract(ECO_TOKEN, signer);
+    const erc20 = Relayer.#getContract(signer);
     const address = await signer.getAddress();
     const allowance = await erc20.allowance(address, PERMIT2_ADDRESS);
     return allowance.gt(ethers.constants.Zero);
   }
 
-  static #getContract(token, signer) {
-    return new ethers.Contract(token.address, ERC20_PERMIT_TOKEN_ABI, signer.provider);
+  static async #getExecutionForSingleTransfer(signer, to, amount) {
+    const sender = await signer.getAddress();
+    const eco = Relayer.#getContract(signer);
+
+    const { data } = await eco.populateTransaction.transfer(to, amount);
+
+    return {
+      sender: sender,
+      nonce: Relayer.#getNonce(),
+      deadline: getTimestamp(1800).toString(),
+      conditions: [],
+      operations: [{ to: ECO_TOKEN.address, data }],
+      payment: { token: ECO_TOKEN.address, amount: SPONSOR_FEE_AMOUNT.toHexString() },
+      tokens: [{ token: ECO_TOKEN.address, amount: amount.add(SPONSOR_FEE_AMOUNT).toHexString() }],
+    };
+  }
+
+  static #getNonce() {
+    return ethers.BigNumber.from(ethers.utils.randomBytes(32)).toHexString();
+  }
+
+  static #getContract(signer) {
+    return new ethers.Contract(ECO_TOKEN.address, ERC20_PERMIT_TOKEN_ABI, signer.provider);
   }
 
   static #getUrl(path) {
@@ -175,7 +229,7 @@ export const useGasless = signer => {
     setError(null);
     setEnabling(true);
     try {
-      await Relayer.initialize("eco", signer);
+      await Relayer.initialize(signer);
       setEnabled(true);
     } catch (e) {
       console.error("[gasless:error:enable]", e);
@@ -189,15 +243,16 @@ export const useGasless = signer => {
   const transfer = async (to, amount) => {
     if (loading) return;
     setError(null);
+    setLastTx(null);
     setLoading(true);
     try {
       // If gasless feature is not enabled, enable it
       if (!enabled) await enable();
 
-      const { transaction } = await Relayer.transfer("eco", signer, to, amount);
+      const { transaction } = await Relayer.sponsorTransfer(signer, to, amount);
       setLastTx({ type: "transfer", transaction });
     } catch (e) {
-      console.error("[gasless:error:enable]", e);
+      console.error("[gasless:error:transfer]", e);
       setError(e.message);
     }
     setLoading(false);
