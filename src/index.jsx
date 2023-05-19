c86438fdc98922128043f778cd5b4faf9b54e090
Carlos Febres <carlosfebres@dorg.tech>
2023-05-19 17:38:38 -0400

    fix context

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { EthersAppContext } from "eth-hooks/context";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "./index.css";
 
 import App from "./App";
 
+const queryClient = new QueryClient();
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <EthersAppContext>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </EthersAppContext>
+    </QueryClientProvider>
   </BrowserRouter>,
 );
