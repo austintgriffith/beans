37e7237cea154a923f2f1fa40599a8e340cce800
Carlos Febres <carlosfebres@dorg.tech>
2023-06-12 16:22:04 -0400

    use REACT_APP_STACKUP_API_KEY env

--- src/constants/stackup.ts
+++ src/constants/stackup.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-export const STACKUP_API_KEY = process.env.STACKUP_API_KEY!;
+export const STACKUP_API_KEY = process.env.REACT_APP_STACKUP_API_KEY!;
 export const VERIFYING_PAYMASTER_ADDRESS = ethers.utils.getAddress("0xE93ECa6595fe94091DC1af46aaC2A8b5D7990770");
 
 export interface ExecutionResult {
